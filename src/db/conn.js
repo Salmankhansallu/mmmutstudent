dc7f881cec9016f895d4a477d03149fca26a7db7
Salmankhansallu <78694695+Salmankhansallu@users.noreply.github.com>
2021-11-03 17:03:06 +0530

    second commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/dynamicregistration",{
+mongoose.connect(process.env.mongodb, {
     useNewUrlParser:true,
     useUnifiedTopology:true
     
